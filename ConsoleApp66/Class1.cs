6a63bdeae60cf4c5e3a882a84d7c3e9cb6ea4fe5
Vikkans <143171920+Vikkans@users.noreply.github.com>
2024-04-03 10:38:00 +0200

    Add project files.

---
+++ ConsoleApp66/Class1.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp66
+{
+    class bil
+    {
+        public string märke;
+        public string modell;
+        public string färg;
+        public int mil;
+        public int årsmodell;
+
+        public void mymetod()
+        {
+            bil bil = new bil();
+
+            Console.WriteLine("Märke?");
+            bil.märke = Console.ReadLine();
+            Console.WriteLine("modell?");
+            bil.modell = Console.ReadLine();
+            Console.WriteLine("färg?");
+            bil.färg = Console.ReadLine();
+            Console.WriteLine("hur många mil har den kört?");
+            bil.mil = int.Parse(Console.ReadLine());
+            Console.WriteLine("årsmodell?");
+            bil.årsmodell = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Du har en " + bil.märke + " " + bil.modell + " " + bil.årsmodell + " din bil är " + bil.färg + " och har kört " + bil.mil + " mil");
+        }
+
+
+
+    }
+}
---
+++ ConsoleApp66/Program.cs
@@ -0,0 +1,15 @@
+﻿using ConsoleApp66;
+using System;
+
+namespace vvovv
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            bil bil = new bil();
+
+            bil.mymetod();
+        }
+    }
+}
